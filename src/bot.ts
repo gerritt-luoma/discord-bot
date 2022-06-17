11683dc72b8d7efa1686480be0701a9254a7baac
gerritt-luoma <gerritt.luoma@gmail.com>
2022-06-16 21:53:16 -0600

    Added more, need to figure out commands

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,7 @@
-import { Client, Intents } from "discord.js";
+import { Client, Collection, Intents } from "discord.js";
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
@@ -7,6 +9,17 @@ const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS
   ]
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+
+commandFiles.forEach(file => {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
 })
 
 client.once('ready', () => {
